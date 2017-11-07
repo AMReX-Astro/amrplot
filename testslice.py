03dcc4c91ba15c55ffd58d60e9677cfb70932e97
Michael Zingale <michael.zingale@stonybrook.edu>
2017-11-07 14:10:04 -0500

    updates

--- testslice.py
+++ testslice.py
@@ -1,16 +1,16 @@
 import yt
 from yt.units import cm
 
-ds = yt.load("plt06677")
+ds = yt.load("sedov_2d_sph_in_cyl_plt00120")
 
 print(ds.domain_left_edge)
 print(ds.domain_right_edge)
 
 xctr = 0.5*(ds.domain_left_edge[0] + ds.domain_right_edge[0])
-yctr = 5000*cm
+yctr = 0.5*(ds.domain_left_edge[1] + ds.domain_right_edge[1])
 
-xwidth = ds.domain_right_edge[0] - ds.domain_left_edge[0]
-ywidth = 1.e4*cm
+xwidth = 0.5*(ds.domain_right_edge[0] - ds.domain_left_edge[0])
+ywidth = 0.5*(ds.domain_right_edge[1] - ds.domain_left_edge[1])
 
 print(yctr - 0.5*ywidth, yctr + 0.5*ywidth)
 
@@ -18,7 +18,7 @@ print(yctr - 0.5*ywidth, yctr + 0.5*ywidth)
 #                   center=(xctr,yctr,0.0), width=(xwidth,ywidth))
 
 slc = yt.plot_2d(ds, "temperature", origin="native",
-                   center=(xctr,yctr), width=(xwidth,ywidth))
+                 center=(xctr,yctr), width=(xwidth,ywidth))
 
 slc.save("test.png")
 
